1765c47d82e2a84812012053b609968eb4a014a8
KateStef <115612557+KateStef@users.noreply.github.com>
2022-10-12 15:15:12 +0800

    Add files via upload

---
+++ data/GEOTAG_5.js
@@ -0,0 +1,1 @@
+var json_GEOTAG_5 = {"type":"FeatureCollection","name":"GEOTAG_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"NAME":"MT APO","Folder":"C:\\RLGM\\GEOTAG\\DENR_IMG_1122018_030639 PM.jpg"},"geometry":{"type":"Point","coordinates":[125.379533491061693,6.890943590164257]}}]}
